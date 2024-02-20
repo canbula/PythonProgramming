045f0f68a92baf8758251cca348c82e6e11ddfb3
UNBahri <140695037+UNBahri@users.noreply.github.com>
2024-02-20 20:44:32 +0300

    Create types_bahri_un.py
    I have read the [CONTRIBUTING] ✓
    I have performed self-review on my own code ✓
    I have run the code locally and it works as expected ✓
    I have commented my code ✓

---
+++ Week02/types_bahri_un.py
@@ -0,0 +1,25 @@
+my_int= 10
+my_var = 123
+
+a =float(5784.2369)
+my_float =2403.5940
+my_float2=2158546548.43
+
+my_bool= True
+my_complex= 87+3j
+
+print("There are integers:")
+print("my_int:",my_int,", type:", type(my_int))
+print("my_var:",my_var, ", type:", type(my_var))
+
+print("There are floats:")
+print("a:",a,"type:", type(a))       
+print("my_float:",my_float, ", type:", type(my_float))
+print("my_float2:",my_float2, ", type:", type(my_float2))
+print(float(3)) #To make sure prints as a decimal number.
+
+print("There are boolean and complex:")
+print(my_bool,", type:", type(my_bool))
+print(my_complex, ", type:", type(my_complex))
+
+#We can understand variable types with "type()".
