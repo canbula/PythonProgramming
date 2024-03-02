1417b7b8ff4c3a1e3dddfc87d5f6c00c8053ca0d
MFarid77 <135022513+MFarid77@users.noreply.github.com>
2024-03-02 22:28:34 +0400

    Update sequences_farid_mammadov.py
    ## Describe your changes
     
    
     
    ## Checklist
    - [x] I have read [CONTRIBUTING].
    - [x] I have performed a self-review of my own code.
    - [x] I have run the code locally and it works as explained.
    - [x] I have commented my code, particularly in hard-to-understand areas
     
    ## Screenshots (if appropriate)

--- Week02/sequences_farid_mammadov.py
+++ Week02/sequences_farid_mammadov.py
@@ -5,14 +5,16 @@ my_dict = {
     "name": "farid",
     "surname": "mammadov"
 }
+
 def remove_duplicates(input_list):
     return list(set(input_list))
-def list_counts(input_list):
 
+def list_counts(input_list):
     counts = {}
     for item in input_list:
         counts[item] = counts.get(item, 0) + 1
     return counts
+
 def reverse_dict(input_dict):
     return {value: key for key, value in input_dict.items()
            }
