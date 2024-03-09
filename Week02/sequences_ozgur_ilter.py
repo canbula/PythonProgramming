4a3b26058de49b1da7d2b7f19af19a408ce3ef74
ozgurilter <147427263+ozgurilter@users.noreply.github.com>
2024-03-09 18:36:01 +0300

    Create sequences_ozgur_ilter.py

---
+++ Week02/sequences_ozgur_ilter.py
@@ -0,0 +1,20 @@
+my_list = [12 ,'ozgur', 75 , True]
+my_tuple = (12,1.5, 'python')
+my_set = {'python', 'java', 'javascript'} 
+my_dict = {
+    'name' : 'Ozgur',
+    'surname': 'Ilter',
+    'country' : 'Turkey'
+}
+
+def remove_duplication(a_list):
+    return list(set(a_list))
+
+
+def list_counts(a_list):
+    return {i: my_list.count(i) for i in my_list}
+
+
+def reverse_dict(my_dict):
+     return {v: k for k, v in my_dict.items()}
+
