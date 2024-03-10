646dc26cfa30fa79dcbf0407447737ea22f6129c
Bilal Ayakdaş <72439364+Bilal-AYAKDAS@users.noreply.github.com>
2024-03-11 00:36:43 +0300

    Create functions_bilal_ayakdas.py

---
+++ Week03/functions_bilal_ayakdas.py
@@ -0,0 +1,34 @@
+custom_power = lambda x=0,/, e=1: x ** e
+
+def custom_equation(x: int = 0, y: int = 0, /, a : int = 1, b : int = 1, *, c: int = 1 )->float:
+    """
+    This function
+    :param x:The first number, default is 0
+    :type x: int
+    :param y:The second number, default is 0
+    :type y: int
+    :param a:The third number, default is 1
+    :type a: int
+    :param b:The fourth number, default is 1
+    :type b: int
+    :param c:The fifth number, default is 1
+    :type c: int
+    :return:The sum of 'x' power 'a' and 'y' power 'b' divided by c
+    :rtype:float
+    """
+    return (x**a + y**b) / c
+  
+def fn_w_counter() -> (int, dict[str, int]):
+    if not hasattr(fn_w_counter, "call_counter"):
+        fn_w_counter.call_counter = 0
+        fn_w_counter.caller_counts = {}
+
+    caller_name = __name__
+    fn_w_counter.call_counter += 1
+
+    if caller_name in fn_w_counter.caller_counts:
+        fn_w_counter.caller_counts[caller_name] += 1
+    else:
+        fn_w_counter.caller_counts[caller_name] = 1
+    return fn_w_counter.call_counter, fn_w_counter.caller_counts
+
