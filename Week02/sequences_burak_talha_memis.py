53db9f3864fac080ee58617178d7a2b04f81a3b8
Burak Talha Memiş <130248799+Buraktalhaa@users.noreply.github.com>
2024-03-12 15:54:03 +0300

    Update sequences_burak_talha_memis.py

--- Week02/sequences_burak_talha_memis.py
+++ Week02/sequences_burak_talha_memis.py
@@ -1,4 +1,4 @@
-my_list = [1,2,3]
+my_list = [1,2,2,3,3,3]
 
 my_tuple = ("tuple1","tuple2")
 
@@ -6,13 +6,14 @@ my_set = {1,1,2,2,3,3,4,5}
 
 my_dict = {"name" : "Burak" , "surname" : "Memis"}
 
-def remove_duplicates(list):
+def remove_duplicates(my_list):
     new_list = []
     for i in my_list:
         if i not in new_list:
             new_list.append(i)
     return new_list        
                    
+
 def list_counts(my_list):
     return {i : my_list.count(i) for i in my_list} 
 
