26b62d4e4e44e4e42a363898271f81bd870f3984
Burak Talha Memiş <130248799+Buraktalhaa@users.noreply.github.com>
2024-03-12 16:05:36 +0300

    Update functions_burak_talha_memis.py

--- Week03/functions_burak_talha_memis.py
+++ Week03/functions_burak_talha_memis.py
@@ -1,14 +1,22 @@
 custom_power = lambda x = 0,/,e = 1: x**e
 
 def custom_equation(x = 0,y = 0,/,a = 1,b = 1, * ,c = 1)->float:
+    """
+    This function
+    :param x:The first number, default is 0
+    :type x: int
+    :param y:The second number, default is 0
+    :type y: int
+    :param a:The third number, default is 1
+    :type a: int
+    :param b:The fourth number, default is 1
+    :type b: int
+    :param c:The fifth number, default is 1
+    :type c: int
+    :return:The sum of 'x' power 'a' and 'y' power 'b' divided by c
+    :rtype:float
+    """   
     return (x**a + y**b) / c
-'''
-x is positional-only with default value 0
-y is positional-only with default value 0
-a is positional-or-keyword with default value 1
-b is positional-or-keyword with default value 1
-c is keyword-only with default value 1
-'''    
 
 def fn_w_counter() -> (int, dict[str,int]):
     if not hasattr(fn_w_counter, 'counter'):
@@ -23,4 +31,5 @@ def fn_w_counter() -> (int, dict[str,int]):
             fn_w_counter.callers[__name__] += 1
         else:
             fn_w_counter.callers[__name__] = 1
+
     
