245769172a9873d03420b936e326d0a069e308a6
zeyneprk <161594609+zeyneprk@users.noreply.github.com>
2025-03-17 12:48:33 +0300

    Create info_types_zeynepreyyan_kaleli.py

---
+++ Week02/info_types_zeynepreyyan_kaleli.py
@@ -0,0 +1,18 @@
+student_id = "220315071"
+full_name = "Zeynep Reyyan Kaleli"
+
+print("Student ID : ", student_id)
+print("Full Name : ",full_name)
+
+
+my_int = 73
+my_float = 9.13
+my_bool = False
+my_complex = 5 + 3j
+
+
+
+print("integer: ",my_int)
+print("float: ",my_float)
+print("boolean : ",my_bool)
+print("complex : ",my_complex)
