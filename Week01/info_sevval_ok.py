a9d4e809c397f2c353c8e54378d6d50cca6600ec
oksevval <119163171+oksevval@users.noreply.github.com>
2025-09-30 14:36:15 +0300

    Create info_sevval_ok.py

---
+++ Week01/info_sevval_ok.py
@@ -0,0 +1,6 @@
+student_id = 210315079
+student_name_surname = Şevval Ok
+email_address = 210315079@ogr.cbu.edu.tr
+print(f"Öğrenci ID : {student_id} (Tip : {type(student_id)})")
+print(f"Tam Ad: {student_name_surname} (Tip: {type(student_name_surname)})")
+print(f"E-posta: {email_address} (Tip: {type(email_address)})")
