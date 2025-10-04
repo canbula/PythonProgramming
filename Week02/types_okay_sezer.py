bab334876244cb1acb20061e956c1b7f903ebd98
OkaySezer <okaysezer581@gmail.com>
2025-10-04 12:54:55 +0300

    Update types_okay_sezer.py

--- Week02/types_okay_sezer.py
+++ Week02/types_okay_sezer.py
@@ -1,4 +1,4 @@
-int my_int = 35 #integer
-float my_float = 25,5 #float
-bool my_bool = True #boolean
-complex my_complex = 3 + 4j #complex
+my_int = 35 #integer
+my_float = 25,5 #float
+my_bool = True #boolean
+my_complex = 3 + 4j #complex
