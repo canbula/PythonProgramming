456f6f864dbd131c1fa9ad597268499687dde1c2
Fevzi Bağrıaçık <124450541+fevzibagriacik@users.noreply.github.com>
2025-11-04 14:32:49 +0300

    Create pyramid_fevzi_bagriacik.py

---
+++ Week03/pyramid_fevzi_bagriacik.py
@@ -0,0 +1,15 @@
+def calculate_pyramid_height(number_of_blocks):
+    height = 0
+    total = 0
+
+    for i in range(1,number_of_blocks+2,1):
+
+        total += i
+
+        if(number_of_blocks <= total):
+            height = i
+            break
+
+        
+    
+    return height
