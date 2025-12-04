76e73bd4e07a0789b839b654953b91161fd67dd5
protagonist9 <bozgan.tarik@gmail.com>
2025-12-04 18:37:15 +0300

    Week04 homeworks are done.

--- Week04/functions_tarik_bozgan.py
+++ Week04/functions_tarik_bozgan.py
@@ -14,8 +14,19 @@ def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int
     """
     return (x**a + y**b) / c
 
-def fn_w_counter(_calls={"__main__": 0}):
-    _calls["__main__"] += 1
-    return _calls["__main__"], dict(_calls)
+def fn_w_counter() -> tuple[int, dict[str, int]]:
+ 
+    caller = __name__
+
+    if not hasattr(fn_w_counter, "total"):
+        fn_w_counter.total = 0
+    if not hasattr(fn_w_counter, "callers"):
+        fn_w_counter.callers = {}
+
+    fn_w_counter.total += 1
+    fn_w_counter.callers[caller] = fn_w_counter.callers.get(caller, 0) + 1
+
+    return fn_w_counter.total, fn_w_counter.callers.copy()
+
 
 
