2c585585701a7037d4905277fd05e98591447355
Fevzi Bağrıaçık <124450541+fevzibagriacik@users.noreply.github.com>
2025-12-08 23:52:03 +0300

    Update functions_fevzi_bagriacik.py

--- Week04/functions_fevzi_bagriacik.py
+++ Week04/functions_fevzi_bagriacik.py
@@ -2,9 +2,8 @@ import inspect
 
 custom_power = lambda x = 0, /,  e = 1 : x ** e
 
-def custom_equation(x=0, y=0, /, a=1, b=1, *, c=1) -> float:
-    """
-    This function calculates (x ** a + y ** b) / c.
+def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int = 1) -> float:
+    """This function calculates (x ** a + y ** b) / c.
     
     :param x: First number (positional only, default 0)
     :param y: Second number (positional only, default 0)
@@ -13,10 +12,9 @@ def custom_equation(x=0, y=0, /, a=1, b=1, *, c=1) -> float:
     :param c: Fifth number (keyword only, default 1)
     :return: (x ** a + y ** b) / c
     """
+    return float((x + y + a + b) / c)
 
-    return (x ** a + y ** b) / c
-
-def fn_w_counter() -> (int, dict):
+def fn_w_counter() -> (int, dict[str, int]):
     if not hasattr(fn_w_counter, 'total_count'):
         fn_w_counter.total_count = 0
         fn_w_counter.caller_dict = {}
