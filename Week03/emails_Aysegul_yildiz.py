f79134bc853561718b43074d5007401e76bb0081
aysegulyildizz <139223718+aysegulyildizz@users.noreply.github.com>
2025-12-23 10:51:19 +0300

    Create Emails class for email validation and uniqueness
    Implement Emails class to manage and validate a list of unique email addresses.

---
+++ Week03/emails_Aysegul_yildiz.py
@@ -0,0 +1,29 @@
+class Emails(list):
+    def __init__(self, emails):
+        self.validate(emails)
+
+
+        unique_emails = list(set(emails))
+
+        super().__init__(unique_emails)
+        self.data = unique_emails
+
+    def validate(self, emails):
+        if not isinstance(emails, list):
+            raise ValueError
+
+        email_regex = re.compile(
+            r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        )
+
+        for email in emails:
+            if not isinstance(email, str):
+                raise ValueError
+            if not email_regex.match(email):
+                raise ValueError
+
+    def __repr__(self):
+        return f"Emails({list(self)})"
+
+    def __str__(self):
+        return "\n".join(self)
